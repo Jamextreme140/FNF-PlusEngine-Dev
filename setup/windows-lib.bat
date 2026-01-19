a7bb5d0692b6f7aeb7794d5c655d82bcc134594d
Lenin <medinastoleninliu@gmail.com>
2026-01-19 00:32:45 -0500

    al parecer, va mal pipipi

--- setup/windows-lib.bat
+++ setup/windows-lib.bat
@@ -4,7 +4,7 @@ cd ..
 echo Installing libraries...
 echo This might take a few moments depending on your internet speed.
 haxelib git hxcpp https://github.com/kittycathy233/hxcpp --quiet
-haxelib git lime https://github.com/Psych-Plus-Team/lime.git --quiet
+haxelib git lime https://github.com/kittycathy233/lime --quiet
 haxelib install extension-androidtools 2.2.2 --quiet
 haxelib install openfl 9.5.0 --quiet
 haxelib install flixel 5.6.2 --quiet
